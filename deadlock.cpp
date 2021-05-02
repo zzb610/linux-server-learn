c502e509999e074f98bdd4e44eb25145085e8cd4
zzb610 <bwyuchi@163.com>
2021-05-02 13:56:03 +0800

    deadlock

---
+++ deadlock.cpp
@@ -0,0 +1,44 @@
+#include <pthread.h>
+#include <cstdio>
+#include <unistd.h>
+int a = 0;
+int b = 0;
+pthread_mutex_t mutex_a;
+pthread_mutex_t mutex_b;
+
+void* another(void* arg)
+{
+    pthread_mutex_lock(&mutex_b);
+    printf("in child thread, got mutex b, waiting for mutex a\n");
+    sleep(5);
+    ++b;
+    pthread_mutex_lock(&mutex_a);
+    b += a++;
+    pthread_mutex_unlock(&mutex_a);
+    pthread_mutex_unlock(&mutex_b);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t id;
+
+    pthread_mutex_init(&mutex_a, NULL);
+    pthread_mutex_init(&mutex_b, NULL);
+    pthread_create(&id, NULL, another, NULL);    
+
+    pthread_mutex_lock(&mutex_a);
+    printf("in parent thread, got mutex a, waiting fot mutex b\n");
+    sleep(5);
+    ++a;
+    pthread_mutex_lock(&mutex_b);
+    a += b++;
+    pthread_mutex_unlock(&mutex_b);
+    pthread_mutex_unlock(&mutex_a);
+
+    pthread_join(id, NULL);
+    pthread_mutex_destroy(&mutex_a);
+    pthread_mutex_destroy(&mutex_b);
+
+    return 0;
+}
